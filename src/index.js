d4ae5bbec7ba85ad8a93ba69d57da952b0179702
mathiasthomas <mathiasthomas17@gmail.com>
2024-08-29 20:50:58 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,134 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+// Header Component
+function Header() {
+  return (
+    // Inline Styling
+    <header className="header">
+      <h1>Mashariki Foods' CO.</h1>
+    </header>
+  );
+}
+// Menu Comp
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaOBJ={pizza} key={pizza.key} />
+        ))}
+      </ul>
+      {/* <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        price={15}
+        photoName="pizzas/focaccia.jpg"
+      />
+      <Pizza
+        name="Mashrooms"
+        ingredients="Tomato Mashes"
+        price={10}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+// Pizza Component
+function Pizza(props) {
+  // console.log(props);
+  // Rule 1 - Func name must strat with capital
+  //Func must return only 1 element
+  return (
+    <li className="pizza">
+      <img src={props.pizzaOBJ.photoName} alt={props.pizzaOBJ.name} />
+      <div>
+        <h3>{props.pizzaOBJ.name}</h3>
+        <p>{props.pizzaOBJ.ingredients}</p>
+        <span>{props.pizzaOBJ.price + 8}</span>
+      </div>
+    </li>
+  );
+}
+
+// Footer Comp
+function Footer() {
+  const hour = new Date().getHours();
+  const openHours = 4;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHours && hour <= closeHour;
+  console.log(hour);
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      {" "}
+      {new Date().toLocaleString()} We are Now Open
+    </footer>
+  );
+}
+
+// React V18
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
